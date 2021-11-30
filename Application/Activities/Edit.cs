cb7a9fcc95d44fba0216fa6920da9793dbce2777
Khalid Aqeeli <aqeelikh@gmail.com>
2021-11-30 11:59:58 +0300

    using AutoMapper

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -6,29 +7,36 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command: IRequest {
+        public class Command : IRequest
+        {
 
             public Activity Activity { get; set; }
-            
+
         }
         public class Handler : IRequestHandler<Command>
-            {
+        {
             private readonly DataContext context;
+            private readonly IMapper mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                this.mapper = mapper;
                 this.context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-                {
+            {
 
-                    var activity = await context.Activities.FindAsync(request.Activity.Id);
-                    activity.Title = request.Activity.Title ?? activity.Title;
-                    // context.Activities.Update(request.Activity);
-                    await context.SaveChangesAsync();
-                    return Unit.Value;
-                }
+                var activity = await context.Activities.FindAsync(request.Activity.Id);
+
+                /* Map the activity that we got from the request to the activity that we have in context(our database)
+                by using AutoMapper we don't have to add all of the object attribute AutoMapper will handle it */
+                mapper.Map(request.Activity,activity);
+
+                await context.SaveChangesAsync();
+
+                return Unit.Value;
             }
+        }
     }
 }
