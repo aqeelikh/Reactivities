de4e44b10e77662f720161b6bc12133c4da61fbf
Khalid Aqeeli <aqeelikh@gmail.com>
2021-11-30 14:28:44 +0300

    refactoring code

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using MediatR;
 using Application.Activities;
 using AutoMapper;
 using Application.core;
+using API.Extensions;
 
 namespace API
 {
@@ -34,25 +35,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors( opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            // tell the services were to find mediator
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(this.config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
